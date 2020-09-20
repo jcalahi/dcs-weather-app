4cdd01a0cdcca6b661d68b6a38ca85e0d6c9ee78
jcalahi <juliuscalahi@gmail.com>
2020-09-20 18:01:06 +0800

    added basename and removed history.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
-import history from './history';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // components
 import WeatherHome from './containers/WeatherHome';
 import WeatherDetail from './containers/WeatherDetail';
 
 function App() {
   return (
-    <Router history={history}>
+    <BrowserRouter basename={window.location.pathname || ''}>
       <Switch>
         <Route exact path="/" component={WeatherHome} />
         <Route exact path="/details" component={WeatherDetail} />
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
 
