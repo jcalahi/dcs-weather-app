abca4441c9fcb79b4746b5b45a7d0450161f4f57
jcalahi <juliuscalahi@gmail.com>
2020-09-27 14:51:45 +0800

    Updates

---
+++ precache-manifest.9561f19c75c5c12fdc8ab721accdaa26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f453a832bc69b8dba97d4fd41c34d0",
+    "url": "/dcs-weather-app/index.html"
+  },
+  {
+    "revision": "8b5b54fbf06f51861a88",
+    "url": "/dcs-weather-app/static/css/main.4851ecba.chunk.css"
+  },
+  {
+    "revision": "ad7fd1d6de0bb19a1f18",
+    "url": "/dcs-weather-app/static/js/2.0a2675ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dcs-weather-app/static/js/2.0a2675ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5b54fbf06f51861a88",
+    "url": "/dcs-weather-app/static/js/main.f7175825.chunk.js"
+  },
+  {
+    "revision": "40da8dbc9d1d390baf2a",
+    "url": "/dcs-weather-app/static/js/runtime-main.1ecf6f2e.js"
+  }
+]);
