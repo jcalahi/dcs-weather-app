a09c6594480212660d1c4c4c155f49e520b17d50
jcalahi <juliuscalahi@gmail.com>
2020-09-30 19:22:50 +0800

    Updates

--- precache-manifest.04e3e4f1f95ac0d49def9c066ad6747d.js
+++ precache-manifest.04e3e4f1f95ac0d49def9c066ad6747d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "03e0db0d4808e640e046caf315993043",
+    "revision": "b238f43066ce394d44ec9eb149094165",
     "url": "/dcs-weather-app/index.html"
   },
   {
-    "revision": "9acc7de2ff8cb11845b7",
+    "revision": "ec3cd005e07422fe0fb2",
     "url": "/dcs-weather-app/static/css/main.acf2c9dc.chunk.css"
   },
   {
-    "revision": "a747172b1729d52fcc3e",
-    "url": "/dcs-weather-app/static/js/2.f8c1044f.chunk.js"
+    "revision": "ba4e8fc139fdaf030302",
+    "url": "/dcs-weather-app/static/js/2.522c8a2a.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/dcs-weather-app/static/js/2.f8c1044f.chunk.js.LICENSE.txt"
+    "url": "/dcs-weather-app/static/js/2.522c8a2a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9acc7de2ff8cb11845b7",
-    "url": "/dcs-weather-app/static/js/main.c82eec86.chunk.js"
+    "revision": "ec3cd005e07422fe0fb2",
+    "url": "/dcs-weather-app/static/js/main.690c8a3d.chunk.js"
   },
   {
     "revision": "40da8dbc9d1d390baf2a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dcs-weather-app/precache-manifest.5247546247870a024dd31e4185f7136c.js"
+  "/dcs-weather-app/precache-manifest.04e3e4f1f95ac0d49def9c066ad6747d.js"
 );
 
 self.addEventListener('message', (event) => {
